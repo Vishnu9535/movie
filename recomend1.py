0585630dfc1108a8a5ae46b1be2e9028767e0fda
Vishnu9535 <vk9255888@gmail.com>
2023-07-03 02:33:41 +0530

    ....

--- recomend1.py
+++ recomend1.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 credits=pd.read_csv('credits.csv')
 movies=pd.read_csv('movies.csv')
@@ -47,8 +48,32 @@ plt.title("recomended movies")
 tfid = TfidfVectorizer(stop_words='english')
 movies['overview']=movies['overview'].fillna(' ')
 tfid_matrix = tfid.fit_transform(movies['overview'])
-print(tfid_matrix.shape)
+# print(tfid_matrix.shape)
+#then we get cosine similarity scores by this we will be getting similarities between two movies ,thus this can be done through linear kernal
 
+cos_sim = linear_kernel(tfid_matrix, tfid_matrix)
+#cos sim =a.b/|a||b|
+indices = pd.Series(movies.index, index=movies['title']).drop_duplicates()
+#create a dict() of key value pairs of title and its index
+# print(indices)
 
+def get_recommendations(title, cosine_sim=cos_sim):
+    # Get the index of the movie that matches the title
+    idx = indices[title]
 
+    # Get the pairwsie similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
 
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:11]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # top 10 most similar movies
+    return movies['title'].iloc[movie_indices]
+x=get_recommendations('The Dark Knight Rises')
+print(x)
